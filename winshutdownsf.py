7480bb8b7b2290b09ceedc9100ae91059b67f58e
theloghox <65214988+theloghox@users.noreply.github.com>
2024-07-19 23:50:35 -0500

    Update winshutdownsf.py

--- winshutdownsf.py
+++ winshutdownsf.py
@@ -14,7 +14,7 @@ child = pexpect.winpty.spawn(ssh_command)
 
 # Manejar la autenticidad del host
 # child.expect espera 0:para la pregunta 1:si solicita password, 2: si la conexión se cierra y 3: tiempo de espera agotado
-i = child.expect(["Are you sure you want to continue connecting (yes/no/[fingerprint])?", "password:", pexpect.EOF, pexpect.TIMEOUT])
+i = child.expect([r"Are you sure you want to continue connecting \(yes/no/\[fingerprint\]\)?", "password:", pexpect.EOF, pexpect.TIMEOUT])
 
 if i == 0:
     # Si se pregunta sobre la autenticidad del host, responde 'yes'
@@ -33,20 +33,20 @@ elif i in [2, 3]:
 # Ingresar la contraseña
 child.sendline(password)
 
-#Interactuamos con la lista de Sophos
-child.expect("Select Menu Number \[0-7\]:")
+# Interactuamos con la lista de Sophos
+child.expect(r"Select Menu Number \[0-7\]:")
 
-#Seleccionamos la opción 7 para apagar/reiniciar el firewall
+# Seleccionamos la opción 7 para apagar/reiniciar el firewall
 child.sendline("7")
 
 # Esperar la solicitud de apagado o reinicio
-child.expect("Shutdown\(S/s\) or Reboot\(R/r\) Device  \(S/s/R/r\):")
+child.expect(r"Shutdown\(S/s\) or Reboot\(R/r\) Device  \(S/s/R/r\):")
 
 # Enviar la respuesta deseada, por ejemplo, 'S' para Shutdown
 child.sendline("S")
 
-#Interactuar con la CLI
+# Interactuar con la CLI
 child.interact()
 
-#Cerrar sesión SSH
+# Cerrar sesión SSH
 child.close()
